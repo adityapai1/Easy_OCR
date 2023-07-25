dff77f9078d119f3af5a34b26d05b4eb1b6b01da
Aditya Pai <pai.a@somaiya.edu>
2023-07-25 20:01:50 +0530

    New Features Trial

---
+++ initial.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Hardcoded reader list
+reader_list = ["apple", "banana", "orange", "grape", "watermelon"]
+
+def check_words_in_reader_list(text_words):
+    return [word for word in text_words if word in reader_list]
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        if uploaded_file:
+            text = uploaded_file.read().decode("utf-8")
+            text_words = text.split()
+            matching_words = check_words_in_reader_list(text_words)
+            return render_template("result.html", words=matching_words)
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
