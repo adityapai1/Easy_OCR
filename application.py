a39f951b0cf462aa95adda0f424336dc1a2a317c
Aditya B. Pai <105188352+adityapai1@users.noreply.github.com>
2023-07-29 12:11:14 +0530

    All working Features merge (#2)
    * Add list feature
    
    * Update Code for list
    
    * Bug Fix - Removes Unnnecessary folder creation
    
    * imports updated
    
    * Working Code, bugs fixed
    
    * Clean Up
    
    * Pull-Issue
    
    * Pushed Update
    
    * format
    
    * Clean Up
    
    * Formatting
    
    * Format changed
    
    * Format Change
    
    * Add Images
    
    * Add files for testing
    
    * Text file input updated
    
    * Final Working Code
    
    ---------
    
    Co-authored-by: Awani Khodwe <awani.khodwe@spit.ac.in>

--- application.py
+++ application.py
@@ -12,92 +12,78 @@ import re
 
 app = Flask(__name__)
 
-def ocr_program(target_folder,pattern):
+
+def ocr_program(target_folder, patterns):
     reader = easyocr.Reader(['en', 'hi'], gpu=False, quantize=False)
 
-    
+    print("Entered the OCR function")
+    for dirpath, dirnames, filenames in os.walk(target_folder):
+        if "Manual" in dirnames:
+            dirnames.remove("Manual")
 
-    #pattern  = 'namrata'
-    print("Entered the ocr function")
-    for dirpath, dirnames, filenames in os.walk(target_folder): #for Recursive Searching of Images
-
-        if (dirnames == "manual") or (dirnames == pattern) :
-            continue
         counter = 1
-        dump =[]
+        dump = []
+
         for image in filenames:
-            
-            word_list =[]
+            word_list = []
             if (image.endswith('.jpg') or image.endswith('.png') or image.endswith('.jpeg')) and 'tmb' not in image:
                 start_time = time.time()
-                result = reader.readtext(dirpath.replace(".\\", '')+"\\"+image, detail=0, paragraph=False)
+                result = reader.readtext(os.path.join(dirpath, image), detail=0, paragraph=False)
                 word_list.extend(result)
-                
-                flag = False
 
-                for i in result:
+                matched_patterns = [pattern for pattern in patterns if any(re.search(pattern, word.upper(), flags=re.I | re.M | re.X) for word in word_list)]
 
-
-                    if re.search(pattern, i.upper(), flags=re.I|re.M|re.X):
-                        flag = True
-                        break
-
-                    else:
-                        flag = False
-
-                if flag == True:
-                    #os.chdir("/home/varun/temp")
-                    #SpecificDir = dirpath.replace(".\\", '')+"\\"+pattern+"\\"
-                    if not os.path.exists(dirpath.replace(".\\", '')+"\\"+pattern+"\\"):
-                        #os.chdir("..")
-                        os.makedirs(dirpath.replace(".\\", '')+"\\"+pattern+"\\")
-                        print("New folder created successfully!")
-
-                    shutil.copy(dirpath.replace(".\\", '')+"\\"+image, dirpath.replace(".\\", '')+"\\"+pattern+"\\")
-
-
+                if matched_patterns:
+                    for pattern in matched_patterns:
+                        pattern_folder = os.path.join(dirpath, pattern)
+                        if not os.path.exists(pattern_folder):
+                            os.makedirs(pattern_folder)
+                            print(
+                                f"New folder '{pattern}' created successfully!")
+                        shutil.copy(os.path.join(
+                            dirpath, image), pattern_folder)
                 else:
-                    if not os.path.exists(dirpath.replace(".\\", '')+"\\Manual\\"):
-                        os.makedirs(dirpath.replace(".\\", '')+"\\Manual\\")
-                        print("New folder created successfully!")
-
-                    shutil.copy(dirpath.replace(".\\", '')+"\\"+image, dirpath.replace(".\\", '')+"\\Manual\\")
+                    manual_folder = os.path.join(dirpath, "Manual")
+                    if not os.path.exists(manual_folder):
+                        os.makedirs(manual_folder)
+                        print("New folder 'Manual' created successfully!")
+                    shutil.copy(os.path.join(dirpath, image), manual_folder)
 
                 end_time = time.time()
-                difference = end_time-start_time
+                difference = end_time - start_time
 
                 print(f"Time Taken for Image {counter} is {math.ceil(difference)} secs")
-                counter+=1
+                counter += 1
+
             dump.extend(word_list)
-            index1=len(dump)
+            index1 = len(dump)
             dump.insert(index1, image)
-            #dump_deduplicated = np.unique(dump) -- to be checked if deduplication is required
-            #np.savetxt(dirpath+"dump.csv", dump_deduplicated, delimiter="|", fmt='% s')
+
         if len(dump) > 0:
-            with open(target_folder.replace(".\\", '').replace(".", '')+ time.strftime("%d-%m-%Y") + ".txt", "a", encoding='utf-8') as f:
-                #Header1 = "Time Taken for Image Number-" + str(counter) + " Name-" + image + "-is " + str(difference) + " secs"
-                #f.write("%s\n" % Header1)
+            with open(target_folder.replace(".\\", '').replace(".", '') + time.strftime("%d-%m-%Y") + ".txt", "a", encoding='utf-8') as f:
                 for item in dump:
                     f.write("%s\n" % item)
-                    
-
 
 
 @app.route('/', methods=['GET', 'POST'])
-
 def index():
     return render_template("form.html")
 
-@app.route('/result', methods=['GET', 'POST'])
 
+@app.route('/result', methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
         target_folder = request.form['target_folder']
-        global FolderCodeWord
-        FolderCodeWord = request.form['folder_code_word']
-        ocr_program(target_folder , FolderCodeWord)
-        return render_template("success.html", )
+        # folder_code_word = request.form['folder_code_word']
+        global uploaded_file
+        global text_words
+        # Convert the comma-separated string into a list
+        uploaded_file = request.files["folder_code_word"]
+        text = uploaded_file.read().decode("utf-8")
+        patterns_list = [pattern.strip()for pattern in text.split(",")]
 
+        ocr_program(target_folder, patterns_list)
+        return render_template("success.html")
 
 
 
